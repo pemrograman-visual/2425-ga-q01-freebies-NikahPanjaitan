ff36dc6610b2df1a4ffe17aad773b8ae3b0fb2f7
NikahPanjaitan <suchiapanjaitan@gmail.com>
2024-10-09 16:02:45 +0700

    PAIR06

--- X01.java
+++ X01.java
@@ -1,2 +1,36 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24011 - Pedro Simangunsong
+// 12S24041 - Nikah Suchia Panjaitan
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, totalharganya, hargapalingkecil, totalnilaitransaksi;
+
+        hargabuku = Double.parseDouble(input.nextLine());
+        totalharganya = hargabuku;
+        hargapalingkecil = hargabuku;
+        while (hargabuku != 0) {
+            if (hargapalingkecil < hargabuku) {
+                hargapalingkecil = hargapalingkecil;
+            } else {
+                hargapalingkecil = hargabuku;
+            }
+            hargabuku = Double.parseDouble(input.nextLine());
+            totalharganya = totalharganya + hargabuku;
+        }
+        if (totalharganya >= 100) {
+            totalnilaitransaksi = totalharganya - hargapalingkecil;
+        } else {
+            totalnilaitransaksi = totalharganya;
+        }
+        System.out.println(toFixed(totalnilaitransaksi,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
+
